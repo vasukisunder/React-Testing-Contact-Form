8c8e482f4d7141fa48ce3aa67b1f4ffcf9f4e52b
Vasuki Sunder <vasukisunder@Vasukis-MBP.attlocal.net>
2020-04-09 14:31:51 -0700

    done

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,16 +12,18 @@ const ContactForm = () => {
 
   return (
     <div className="App">
+      <h1 >React Testing Contact Form</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
             name="firstName"
+            id = "firstName"
             placeholder="bill"
             ref={register({ required: true, maxLength: 3 })}
           />
           {errors.firstName && (
-            <p>Looks like there was an error: {errors.firstName.type}</p>
+            <p data-testid = 'error'>Looks like there was an error: {errors.firstName.type}</p>
           )}
         </div>
 
@@ -30,6 +32,7 @@ const ContactForm = () => {
           <input
             name="lastName"
             placeholder="luo"
+            id = "lastName"
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -41,21 +44,21 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" id="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea name="message" id ="message" ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid = 'submit' type="submit" />
       </form>
     </div>
   );
